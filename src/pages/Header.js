442b6ef9b8c074147cdd14aed1738c4b5412ddfd
Yonas Alem <yonalem21@gmail.com>
2021-12-08 17:49:14 +0300

    header and section pages are added

---
+++ src/pages/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="App-header">
+      <ul className="nav  nav-fill">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Active
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Much longer nav link
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
