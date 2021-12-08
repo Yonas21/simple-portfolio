2dc0e7787a63eb60d41fd5b6caf2e9560140a4ca
Yonas Alem <yonalem21@gmail.com>
2021-12-08 22:06:33 +0300

    readme update

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { routes } from "../constants/routes";
 
 function Header() {
   return (
     <div className="App-header">
       <ul className="nav  nav-fill">
         <li className="nav-item">
-          <Link className="nav-link active" to="/">
+          <Link className="nav-link active" to={routes.HOME}>
             Home
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/contact">
+          <Link className="nav-link" to={routes.CONTACT}>
             Contact Me
           </Link>
         </li>
         {/* <li className="nav-item">
-          <Link className="nav-link" to="/about">
+          <Link className="nav-link" to={routes.ABOUT}>
             About
           </Link>
         </li> */}
